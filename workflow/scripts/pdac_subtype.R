ccf569ae791aa1231ad4e49afce713d71aa87f6f
dtklinh <dangtruongkhanhlinh@gmail.com>
2025-11-09 21:52:46 +0100

    update pdac subtype script

--- workflow/scripts/pdac_subtype.R
+++ workflow/scripts/pdac_subtype.R
@@ -25,6 +25,31 @@ Gene_length <- Mx %>%
   pull(Length)
 CountTable <- Mx %>% 
   select(-c(2:6)) %>% 
-  remove_rownames() %>% 
-  column_to_rownames(var = "Geneid") %>% 
+  #remove_rownames() %>% 
+  #column_to_rownames(var = "Geneid") %>% 
   rename_with(~ str_replace_all(., "-.*", ""))
+
+biomarkers = c("GPR87", "REG4", 
+               "KRT6A", "ANXA10",
+               "BCAR3", "GATA6",
+               "PTGES", "CLDN18",
+               "ITGA3", "LGALS4",
+               "C16orf74", "DDC", 
+               "S100A2", "SLC40A1", 
+               "KRT5", "CLRN3")
+  
+### TPM Normalization
+# Step 1: Calculate Reads Per Kilobase (RPK)
+rpk <- CountTable / (Gene_length / 1000)
+
+# Step 2: Calculate per-sample scaling factor (sum of RPKs)
+scaling_factors <- colSums(rpk)
+
+# Step 3: Calculate TPM
+CountTable_TPM <- t(t(rpk) / scaling_factors * 1e6)
+
+CountTable_TPM_mini <- CountTable_TPM %>% 
+  as.data.frame() %>% 
+  rownames_to_column(var = "row_names") %>% 
+  filter(row_names %in% biomarkers) %>% 
+  column_to_rownames(var = "row_names")
